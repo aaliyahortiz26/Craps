fb3be01ec148f383764b82c2f50021b34d02ab9d
Aaliyah <ortizaa00@gmail.com>
2020-02-14 13:26:18 -0500

    Added die.

--- die.h
+++ die.h
@@ -5,6 +5,8 @@
 #ifndef CRAPS_DIE_H
 #define CRAPS_DIE_H
 
+#include <string>
+
 class Die{
 private:
     int numberOfSides;
@@ -16,8 +18,8 @@ private:
     int startingValue;
 
 public:
-    Die(int startingNumberofSides, int startingIncrement, int startingStaringValue,
-    std::string color, bool isSymbols = false , std::string material = "Bone");
+    Die(int startingNumberofSides, int startingIncrement = 1, int startingStaringValue = 1,
+    std::string color = "White", bool isSymbols = false , std::string material = "Bone");
 };
 
 #endif //CRAPS_DIE_H
