b98d9c2698d829c80fd466c5bea7ed03bdbb1dcd
Aaliyah <ortizaa00@gmail.com>
2020-02-14 13:13:53 -0500

    Added die.

---
+++ die.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aaliyah Ortiz on 2/14/20.
+//
+
+#include "die.h"
---
+++ die.h
@@ -0,0 +1,15 @@
+//
+// Created by Aaliyah Ortiz on 2/14/20.
+//
+
+#ifndef CRAPS_DIE_H
+#define CRAPS_DIE_H
+
+class Die{
+private:
+    int numberOfSides;
+    int increment;
+    int value;
+};
+
+#endif //CRAPS_DIE_H
