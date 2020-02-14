40add3cd7d91ee155b51463623fa73228b05e2a5
Aaliyah <ortizaa00@gmail.com>
2020-02-14 13:19:49 -0500

    Added die.

--- die.h
+++ die.h
@@ -10,6 +10,13 @@ private:
     int numberOfSides;
     int increment;
     int value;
+    string color;
+    bool isSymbols;
+    string material;
+    int startingValue;
+
+public:
+    Die(int startingNumberofSides, int startingIncrement, int startingStaringValue);
 };
 
 #endif //CRAPS_DIE_H
